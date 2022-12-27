c8a565c900df40b67bb8795e11acb8dc5b4987f7
Gleb Natapov <gleb@scylladb.com>
2022-12-27 13:31:24 +0200

    storage_service: raft topology: introduce a function that applies topology cmd to local state machine
    The function applies to persistent storage and call stub function
    topology_change_state_load() that will load the new state into the
    memory in later patches.

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -763,6 +763,7 @@ public:
     bool is_repair_based_node_ops_enabled(streaming::stream_reason reason);
 
 private:
+    friend class group0_state_machine;
     // This fibers monitors raft state and start/stops the topology change
     // coordinator fiber
     future<> raft_state_monitor_fiber(raft::server&, sharded<db::system_distributed_keyspace>& sys_dist_ks);
@@ -775,7 +776,10 @@ private:
 
     future<raft_topology_cmd_result> raft_topology_cmd_handler(sharded<db::system_distributed_keyspace>& sys_dist_ks, raft::term_t term, const raft_topology_cmd& cmd);
 
-public:
+    // This is called on all nodes for each new command received through raft
+    future<> topology_transition(storage_proxy& proxy, gms::inet_address, std::vector<canonical_mutation>);
+    // load topology state machine snapshot into memory
+    future<> topology_state_load();
     // Applies received raft snapshot to local state machine persistent storage
     future<> merge_topology_snapshot(raft_topology_snapshot snp);
 };
