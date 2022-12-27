783466f7125aa0d14b1358de2078761a1a2dcec8
Gleb Natapov <gleb@scylladb.com>
2022-12-27 12:32:31 +0200

    storage_service: raft topology: introduce snapshot transfer code for the topology table

--- service/topology_change_sm.hh
+++ service/topology_change_sm.hh
@@ -23,6 +23,7 @@
 #include "utils/UUID.hh"
 #include "utils/rjson.hh"
 #include "dht/i_partitioner.hh"
+#include "canonical_mutation.hh"
 
 namespace service {
 
@@ -76,6 +77,13 @@ struct topology {
                                     boost::range::join(new_nodes, left_nodes)));
         return boost::find_if(range, [id] (auto& e) { return e.first == id; }) != range.end();
     }
+};
+
+struct raft_topology_snapshot {
+    std::vector<canonical_mutation> mutations;
+};
+
+struct raft_topology_pull_params {
 };
 
 // State machine that is responsible for topology change
