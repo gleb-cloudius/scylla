bcf39fb8024b529dcd7eab53b674184e9e1dff1e
Gleb Natapov <gleb@scylladb.com>
2023-02-23 15:42:49 +0200

    lwt: do not destroy capture in upgrade_if_needed lambda since the lambda is used more then once
    If on the first call the capture is destroyed second call may crash.

--- service/paxos/paxos_state.cc
+++ service/paxos/paxos_state.cc
@@ -115,7 +115,7 @@ future<prepare_response> paxos_state::prepare(storage_proxy& sp, tracing::trace_
                             // for that version and upgrade the mutation with it.
                             logger.debug("Stored mutation references outdated schema version. "
                                 "Trying to upgrade the accepted proposal mutation to the most recent schema version.");
-                            return service::get_column_mapping(p->update.column_family_id(), p->update.schema_version()).then([schema = std::move(schema), p = std::move(p)] (const column_mapping& cm) {
+                            return service::get_column_mapping(p->update.column_family_id(), p->update.schema_version()).then([schema, p = std::move(p)] (const column_mapping& cm) {
                                 return make_ready_future<std::optional<proposal>>(proposal(p->ballot, freeze(p->update.unfreeze_upgrading(schema, cm))));
                             });
                         };
