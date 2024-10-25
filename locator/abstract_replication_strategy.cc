f47f33c05ee5d983d2db824e91507016ac915d41
Sergey Zolotukhin <sergey.zolotukhin@scylladb.com>
2024-10-25 17:33:20 +0200

    Add `effective_replication_map::get_replication_factor` functions.
    Add implementations for new get_replication_factor functions in the
    effective replication map that for Tablets calculate the replication
    factor based on actual replica counts from the Tablets map. This accounts for
    potential inconsistencies between the schema and Tablets during migration.

--- locator/abstract_replication_strategy.cc
+++ locator/abstract_replication_strategy.cc
@@ -17,10 +17,10 @@
 #include <seastar/coroutine/parallel_for_each.hh>
 #include "replica/database.hh"
 #include "utils/stall_free.hh"
-
 #include <boost/icl/interval.hpp>
 #include <boost/icl/interval_map.hpp>
 #include <variant>
+#include "network_topology_strategy.hh"
 
 namespace locator {
 
@@ -588,6 +588,18 @@ effective_replication_map::effective_replication_map(replication_strategy_ptr rs
 
 static_effective_replication_map::factory_key static_effective_replication_map::make_factory_key(const replication_strategy_ptr& rs, const token_metadata_ptr& tmptr) {
     return factory_key(rs->get_type(), rs->get_config_options(), tmptr->get_ring_version());
+}
+
+
+size_t vnode_effective_replication_map::get_replication_factor([[maybe_unused]] dht::token id, const seastar::sstring& datacenter) const {
+    const auto& replication_strategy = get_replication_strategy();
+
+    if (replication_strategy.get_type() == replication_strategy_type::network_topology) {
+        const network_topology_strategy* nrs = static_cast<const network_topology_strategy*>(&replication_strategy);
+        return nrs->get_replication_factor(datacenter);
+    }
+
+    return get_replication_factor(id);
 }
 
 future<static_effective_replication_map_ptr> effective_replication_map_factory::create_static_effective_replication_map(replication_strategy_ptr rs, const token_metadata_ptr& tmptr) {
