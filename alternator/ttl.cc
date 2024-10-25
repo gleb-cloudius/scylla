04d276712f1eb11cb3fda348029588f9673ef727
Sergey Zolotukhin <sergey.zolotukhin@scylladb.com>
2024-10-25 15:29:12 +0200

    Rename `get_replication_factor` to `get_schema_replication_factor`
    As part of introducing a new method for calculating the replication
    factor for Tablets, rename the existing `get_replication_factor` to
    `get_schema_replication_factor`, since it returns the replication
    factor from the replication strategy, whereas the new method will
    calculate the replication factor based on the current state of the
    Tablets.

--- alternator/ttl.cc
+++ alternator/ttl.cc
@@ -780,7 +780,7 @@ static future<bool> scan_table(
             // check if this is the primary replica for the current tablet
             if (tablet_primary_replica.host == my_host_id && tablet_primary_replica.shard == this_shard_id()) {
                 co_await scan_tablet(*tablet, proxy, abort_source, page_sem, expiration_stats, scan_ctx, tablet_map);
-            } else if(erm->get_replication_factor() > 1) {
+            } else if(erm->get_schema_replication_factor() > 1) {
                 // Check if this is the secondary replica for the current tablet
                 // and if the primary replica is down which means we will take over this work.
                 // If each node only scans its own primary ranges, then when any node is
