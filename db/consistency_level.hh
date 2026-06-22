bitpathfinder/scylladb

Modify storage_proxy to use the tablet-aware `get_replication_factor`.
Replace `get_schema_replication_factor` with the new `get_replication_factor`,
which, for tablets, returns the actual number of replicas while accounting
for the transition state of Tablets.

Fixes scylladb/scylladb#20625

db/consistency_level.hh
@@ -12,6 +12,7 @@
 
 #include "db/consistency_level_type.hh"
 #include "db/read_repair_decision.hh"
+#include "dht/token.hh"
 #include "gms/inet_address.hh"
 #include "inet_address_vectors.hh"
 #include "utils/log.hh"
@@ -29,15 +30,15 @@ namespace db {
 
 extern logging::logger cl_logger;
 
-size_t quorum_for(const locator::effective_replication_map& erm);
+size_t quorum_for(const locator::effective_replication_map& erm, dht::token token_id);
 
-size_t local_quorum_for(const locator::effective_replication_map& erm, const sstring& dc);
+size_t local_quorum_for(const locator::effective_replication_map& erm, const sstring& dc, dht::token token_id);
 
-size_t block_for_local_serial(const locator::effective_replication_map& erm);
+size_t block_for_local_serial(const locator::effective_replication_map& erm, dht::token token_id);
 
-size_t block_for_each_quorum(const locator::effective_replication_map& erm);
+size_t block_for_each_quorum(const locator::effective_replication_map& erm, dht::token token_id);
 
-size_t block_for(const locator::effective_replication_map& erm, consistency_level cl);
+size_t block_for(const locator::effective_replication_map& erm, consistency_level cl, dht::token token_id);
 
 bool is_datacenter_local(consistency_level l);
 
@@ -49,7 +50,8 @@ filter_for_query(consistency_level cl,
                  read_repair_decision read_repair,
                  const gms::gossiper& g,
                  std::optional<gms::inet_address>* extra,
-                 replica::column_family* cf);
+                 replica::column_family* cf,
+                 dht::token token_id);
 
 struct dc_node_count {
     size_t live = 0;
@@ -59,16 +61,18 @@ struct dc_node_count {
 bool
 is_sufficient_live_nodes(consistency_level cl,
                          const locator::effective_replication_map& erm,
-                         const inet_address_vector_replica_set& live_endpoints);
+                         const inet_address_vector_replica_set& live_endpoints,
+                         dht::token token_id);
 
 template<typename Range, typename PendingRange = std::array<gms::inet_address, 0>>
 void assure_sufficient_live_nodes(
         consistency_level cl,
         const locator::effective_replication_map& erm,
         const Range& live_endpoints,
+        dht::token token_id,
         const PendingRange& pending_endpoints = std::array<gms::inet_address, 0>());
 
-extern template void assure_sufficient_live_nodes(consistency_level, const locator::effective_replication_map&, const inet_address_vector_replica_set&, const std::array<gms::inet_address, 0>&);
-extern template void assure_sufficient_live_nodes(db::consistency_level, const locator::effective_replication_map&, const inet_address_vector_replica_set&, const utils::small_vector<gms::inet_address, 1ul>&);
+extern template void assure_sufficient_live_nodes(consistency_level, const locator::effective_replication_map&, const inet_address_vector_replica_set&, dht::token token_id, const std::array<gms::inet_address, 0>&);
+extern template void assure_sufficient_live_nodes(db::consistency_level, const locator::effective_replication_map&, const inet_address_vector_replica_set&, dht::token token_id, const utils::small_vector<gms::inet_address, 1ul>&);
 
 }

locator/everywhere_replication_strategy.cc
@@ -40,8 +40,9 @@ void everywhere_replication_strategy::validate_options(const gms::feature_servic
     }
 }
 
-sstring everywhere_replication_strategy::sanity_check_read_replicas(const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas) const {
-    const auto replication_factor = erm.get_schema_replication_factor();
+sstring everywhere_replication_strategy::sanity_check_read_replicas(
+        const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas, const dht::token token) const {
+    const auto replication_factor = erm.get_replication_factor(token);
     if (read_replicas.size() > replication_factor) {
         return seastar::format("everywhere_replication_strategy: the number of replicas for everywhere_replication_strategy is {}, cannot be higher than replication factor {}", read_replicas.size(), replication_factor);
     }

locator/local_strategy.cc
@@ -38,7 +38,8 @@ size_t local_strategy::get_replication_factor(const token_metadata&) const {
     return 1;
 }
 
-sstring local_strategy::sanity_check_read_replicas(const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas) const {
+sstring local_strategy::sanity_check_read_replicas(
+        const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas, const dht::token token) const {
     if (read_replicas.size() > 1) {
         return seastar::format("local_strategy: the number of replicas for local_strategy is {}, cannot be higher than 1", read_replicas.size());
     }

locator/local_strategy.hh
@@ -36,7 +36,8 @@ public:
         return false;
     }
 
-    [[nodiscard]] sstring sanity_check_read_replicas(const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas) const override;
+    [[nodiscard]] sstring sanity_check_read_replicas(
+            const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas, dht::token token) const override;
 };
 
 }

locator/simple_strategy.cc
@@ -79,7 +79,8 @@ std::optional<std::unordered_set<sstring>>simple_strategy::recognized_options(co
     return {{ "replication_factor" }};
 }
 
-sstring simple_strategy::sanity_check_read_replicas(const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas) const {
+sstring simple_strategy::sanity_check_read_replicas(
+        const effective_replication_map& erm, const inet_address_vector_replica_set& read_replicas, const dht::token token) const {
     if (read_replicas.size() > _replication_factor) {
         return seastar::format("ERM inconsistency, the read replica set for simple strategy has higher count of"
                                " read replicas [{}] than its replication factor [{}]",

gleb-cloudius/scylla

db/consistency_level.hh
@@ -12,6 +12,8 @@
 
 #include "db/consistency_level_type.hh"
 #include "db/read_repair_decision.hh"
+#include "dht/token.hh"
+#include "gms/inet_address.hh"
 #include "inet_address_vectors.hh"
 #include "utils/log.hh"
 #include "replica/database_fwd.hh"
@@ -28,15 +30,15 @@ namespace db {
 
 extern logging::logger cl_logger;
 
-size_t quorum_for(const locator::effective_replication_map& erm);
+size_t quorum_for(const locator::effective_replication_map& erm, dht::token token_id);
 
-size_t local_quorum_for(const locator::effective_replication_map& erm, const sstring& dc);
+size_t local_quorum_for(const locator::effective_replication_map& erm, const sstring& dc, dht::token token_id);
 
-size_t block_for_local_serial(const locator::effective_replication_map& erm);
+size_t block_for_local_serial(const locator::effective_replication_map& erm, dht::token token_id);
 
-size_t block_for_each_quorum(const locator::effective_replication_map& erm);
+size_t block_for_each_quorum(const locator::effective_replication_map& erm, dht::token token_id);
 
-size_t block_for(const locator::effective_replication_map& erm, consistency_level cl);
+size_t block_for(const locator::effective_replication_map& erm, consistency_level cl, dht::token token_id);
 
 bool is_datacenter_local(consistency_level l);
 
@@ -48,7 +50,8 @@ filter_for_query(consistency_level cl,
                  read_repair_decision read_repair,
                  const gms::gossiper& g,
                  std::optional<locator::host_id>* extra,
-                 replica::column_family* cf);
+                 replica::column_family* cf,
+                 dht::token token_id);
 
 struct dc_node_count {
     size_t live = 0;
@@ -58,11 +61,16 @@ struct dc_node_count {
 bool
 is_sufficient_live_nodes(consistency_level cl,
                          const locator::effective_replication_map& erm,
-                         const host_id_vector_replica_set& live_endpoints);
+                         const host_id_vector_replica_set& live_endpoints,
+                         dht::token token_id);
 
+template<typename Range, typename PendingRange = std::array<gms::inet_address, 0>>
 void assure_sufficient_live_nodes(
         consistency_level cl,
         const locator::effective_replication_map& erm,
-        const host_id_vector_replica_set& live_endpoints,
-        const host_id_vector_topology_change& pending_endpoints = host_id_vector_topology_change());
+        const Range& live_endpoints,
+        dht::token token_id,
+        const PendingRange& pending_endpoints = std::array<gms::inet_address, 0>());
+
+extern template void assure_sufficient_live_nodes(db::consistency_level, const locator::effective_replication_map&, const host_id_vector_replica_set&, dht::token token_id, const host_id_vector_topology_change&);
 }

locator/everywhere_replication_strategy.cc
@@ -40,8 +40,9 @@ void everywhere_replication_strategy::validate_options(const gms::feature_servic
     }
 }
 
-sstring everywhere_replication_strategy::sanity_check_read_replicas(const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas) const {
-    const auto replication_factor = erm.get_schema_replication_factor();
+sstring everywhere_replication_strategy::sanity_check_read_replicas(
+        const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas, const dht::token token) const {
+    const auto replication_factor = erm.get_replication_factor(token);
     if (const auto& topo_info = erm.get_token_metadata().get_topology_change_info(); topo_info && topo_info->read_new) {
         if (read_replicas.size() > replication_factor + 1) {
             return seastar::format(

locator/local_strategy.cc
@@ -34,7 +34,8 @@ size_t local_strategy::get_replication_factor(const token_metadata&) const {
     return 1;
 }
 
-sstring local_strategy::sanity_check_read_replicas(const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas) const {
+sstring local_strategy::sanity_check_read_replicas(
+        const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas, const dht::token token) const {
     if (read_replicas.size() > 1) {
         return seastar::format("local_strategy: the number of replicas for local_strategy is {}, cannot be higher than 1", read_replicas.size());
     }

locator/local_strategy.hh
@@ -34,7 +34,8 @@ public:
         return false;
     }
 
-    [[nodiscard]] sstring sanity_check_read_replicas(const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas) const override;
+    [[nodiscard]] sstring sanity_check_read_replicas(
+            const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas, dht::token token) const override;
 };
 
 }

locator/simple_strategy.cc
@@ -79,7 +79,8 @@ void simple_strategy::validate_options(const gms::feature_service&, const locato
     }
 }
 
-sstring simple_strategy::sanity_check_read_replicas(const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas) const {
+sstring simple_strategy::sanity_check_read_replicas(
+        const effective_replication_map& erm, const host_id_vector_replica_set& read_replicas, const dht::token token) const {
     if (read_replicas.size() > _replication_factor) {
         return seastar::format("ERM inconsistency, the read replica set for simple strategy has higher count of"
                                " read replicas [{}] than its replication factor [{}]",